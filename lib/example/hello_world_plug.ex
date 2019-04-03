d1cc047f29fedc7f1f984a7d3f9fae8d7b63e96e
Leonardo Victor <leonardovff@gmail.com>
2019-04-03 19:28:16 -0300

    finish the elixir code

--- lib/example/hello_world_plug.ex
+++ lib/example/hello_world_plug.ex
@@ -1,11 +1,20 @@
+defmodule Greeter do
+    def createList(list, i) when i <= 0 do
+        list
+    end 
+    def createList(list, i) do
+        createList(list ++ [:rand.uniform(100000000)], i-1)
+    end
+end
+
 defmodule Example.HelloWorldPlug do
   import Plug.Conn
 
   def init(options), do: options
-
   def call(conn, _opts) do
+    Enum.join(Enum.sort(Greeter.createList([], 15000)))
     conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(200, "Hello World!\n")
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, '{"teste": "oi"}')
   end
 end
